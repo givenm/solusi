25bb0735ad8def8d7778d8befe00d0ae2b6f3cde
given <given@hacker-PC>
2014-02-05 11:55:34 +0200

    added mongodb connection to Cloud Platform

--- src/main/java/zw/ac/solusiuniversity/repository/mongoinitservice/InitMongoService.java
+++ src/main/java/zw/ac/solusiuniversity/repository/mongoinitservice/InitMongoService.java
@@ -4,9 +4,18 @@
  */
 package zw.ac.solusiuniversity.repository.mongoinitservice;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import java.net.ConnectException;
+import java.net.UnknownHostException;
+import javax.servlet.ServletException;
 import zw.ac.solusiuniversity.model.Role;
 import zw.ac.solusiuniversity.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
@@ -20,9 +29,53 @@ public class InitMongoService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+    private Mongo mongo;
+    private DB mongoDB;
 
-    public void init() {
-        // Drop existing collections
+    public void init() throws ServletException {
+        DB db = mongoTemplate.getDb(); 
+        try {
+            DBObject ping = new BasicDBObject("ping", "1");
+            db.command(ping);
+            System.out.println("Connecting to local HOST DB"); //test if we have a connection
+        }
+        catch (Exception  e) {
+            //connect to openshift Mongo DB
+            System.out.println("TRYING TO CONNECT TO OPENSHIFT MONGODB NOW!!!!!!!!");
+            
+            String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST"); 
+            String sport = System.getenv("OPENSHIFT_MONGODB_DB_PORT");
+            String dbName = System.getenv("OPENSHIFT_APP_NAME");
+            if (dbName == null) { 
+                dbName = "testmongo";
+            }
+            
+            String user = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+            String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+            
+            int port = Integer.decode(sport);
+            
+            try {
+                mongo = new Mongo(host, port);
+            }
+            catch (UnknownHostException ue) {
+                throw new ServletException("Failed to access Mongo server", ue);
+            }
+            mongoDB = mongo.getDB(dbName);
+            if (mongoDB.authenticate(user, password.toCharArray()) == false) {
+                throw new ServletException("Failed to authenticate against db: " + db);
+            } else {
+                UserCredentials credentials = new UserCredentials(user, password);
+                MongoTemplate newMongoTemplate = new MongoTemplate(mongo, dbName, credentials);
+                mongoTemplate = newMongoTemplate;
+
+//                ApplicationContext ctx = new GenericXmlApplicationContext("applicationContext-connection.xml");
+//                MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate"); 
+            }
+            System.out.println("Connected to openshift DB");
+        }
+
+// Drop existing collections
         mongoTemplate.dropCollection("role");
         mongoTemplate.dropCollection("user");
 
@@ -30,52 +83,52 @@ public class InitMongoService {
         Role adminRole = new Role.Builder("Administrator")
                 .role(1)
                 .build();
-        
+
         Role userRole = new Role.Builder("User")
                 .role(2)
                 .build();
 
-        User john = new User.Builder("John")   
+        User john = new User.Builder("John")
                 .lastname("Smith")
                 .password("21232f297a57a5a743894a0e4a801fc3")
                 .role(adminRole)
                 .username("john@gmail.com")
                 .build();
-        User given = new User.Builder("Given")   
+        User given = new User.Builder("Given")
                 .lastname("Nyauyanga")
                 .password("ff11cbb19052e40b07aac0ca060c23ee")
                 .role(userRole)
                 .username("given@conycto.com")
                 .build();
-      
-        User jane = new User.Builder("Jane")   
+
+        User jane = new User.Builder("Jane")
                 .lastname("Adams")
                 .password("ee11cbb19052e40b07aac0ca060c23ee")
                 .role(userRole)
                 .username("jane@conycto.com")
                 .build();
-      
-        User elison = new User.Builder("Elison")   
+
+        User elison = new User.Builder("Elison")
                 .lastname("Noraah")
                 .password("2266cbb19052e40b07aac0ca060c23ee")
                 .role(userRole)
                 .username("elison@conycto.com")
                 .build();
-        
-        User laura = new User.Builder("Laura")   
+
+        User laura = new User.Builder("Laura")
                 .lastname("Allimont")
                 .password("ppoo88b19052e40b07aac0ca060c23ee")
                 .role(userRole)
                 .username("laura@gmail.com")
                 .build();
-        
-        User simon = new User.Builder("Simon")   
+
+        User simon = new User.Builder("Simon")
                 .lastname("Saya")
                 .password("2o69cbb19052e40b07aac0ca060c23ee")
                 .role(userRole)
                 .username("simon@gmail.com")
                 .build();
-        
+
         // Insert to db
         mongoTemplate.insert(adminRole, "role");
         mongoTemplate.insert(userRole, "role");
@@ -85,6 +138,6 @@ public class InitMongoService {
         mongoTemplate.insert(elison, "user");
         mongoTemplate.insert(laura, "user");
         mongoTemplate.insert(simon, "user");
-        
+
     }
 }
